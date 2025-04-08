fd7e3e5067b883cde8931c3c237ec1f7bc3949f6
anaisBela <anaisabelasnts@gmail.com>
2025-04-07 23:36:31 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById("root").innerHTML = `
+  <h1 style='color: #4ade80;'>Run&Train</h1>
+  <p>Transforme sua rotina com treinos funcionais e corrida.</p>
+  <button style='background-color: #22c55e; padding: 10px 20px; border: none; color: white;'>Baixar App</button>
+`;
